0404d53f5ca219cd2abef70271855a63c60eeef0
franky3553 <franky3553@gmail.com>
2022-05-14 22:43:36 -0300

    def non starting importExport functions

--- pro3-1.py
+++ pro3-1.py
@@ -5,3 +5,17 @@ startingDeck = []
 parkDeck = []
 raceDeck = []
 winDeck = []
+
+## def non starting importExport functions
+
+def importCSVinDeck(csv, deck):
+   resetDeck(auxiliarDeck)
+   copyCSVinAuxiliarDeck(csv, auxiliarDeck)
+   copyAuxiliarDeckinDeck(auxiliarDeck, deck)
+   resetDeck(auxiliarDeck)
+	
+def exportDeckinCSV(csv, deck):
+   resetDeck(auxiliarDeck)
+   copyDeckinAuxiliarDeck(deck, auxiliarDeck)
+   copyAuxiliarDeckinCSV(auxiliarDeck, csv)
+   resetDeck(auxiliarDeck)
