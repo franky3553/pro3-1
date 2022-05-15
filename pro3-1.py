584d52039b5718b1642f8ad352997a517693f505
franky3553 <franky3553@gmail.com>
2022-05-14 22:59:16 -0300

    def functions inside non starting importExport functions
    def resetDeck(deck)
    
    def copyCSVinAuxiliarDeck(csv, auxiliarDeck)
    
    def copyAuxiliarDeckinDeck(auxiliarDeck, deck)
    
    def copyDeckinAuxiliarDeck(deck, auxiliarDeck)
    
    def copyAuxiliarDeckinCSV(auxiliarDeck, csv)

--- pro3-1.py
+++ pro3-1.py
@@ -19,3 +19,39 @@ def exportDeckinCSV(csv, deck):
    copyDeckinAuxiliarDeck(deck, auxiliarDeck)
    copyAuxiliarDeckinCSV(auxiliarDeck, csv)
    resetDeck(auxiliarDeck)
+
+## def functions inside non starting importExport functions
+
+def resetDeck(deck):
+   for capsule in range(len(deck)):
+      del deck[0]
+# capsule is a section of a deck composed of status line and statmentAnswer line
+
+def copyCSVinAuxiliarDeck(csv, auxiliarDeck):
+   with open("{}.csv".format(csv)) as file:
+      reader = csv.reader(file, delimiter=",")
+      for row in reader:
+            auxiliarDeck.append(row)
+
+def copyAuxiliarDeckinDeck(auxiliarDeck, deck):
+   CSVline = 0
+   linesPerCapsule = 2
+   totalLines = len(csv)
+   totalCapsules = int(totalLines / linesPerCapsule)
+   for capsule in range(totalCapsules):
+      deck.append([])
+      for linea in range(linesPerCapsule):
+         deck[capsule].append(auxiliarDeck[CSVline])         
+         CSVline += 1
+
+def copyDeckinAuxiliarDeck(deck, auxiliarDeck):
+   linesPerCapsule = 2
+   for capsule in range(len(deck)):
+      for line in range(linesPerCapsule):
+         auxiliarDeck.append(deck[capsule][line])
+
+def copyAuxiliarDeckinCSV(auxiliarDeck, csv):
+   with open("{}.csv".format(csv), "w", newline="") as file:
+      writer = csv.writer(file, delimiter=",")
+      writer.writerows(auxiliarDeck)
+      
