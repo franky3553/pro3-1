2db4cc22d05251dbccc8c34a5ec029b32c0200c9
Franky <45402163+franky3553@users.noreply.github.com>
2022-10-18 21:02:03 +0200

    Update code: console clear sintax

--- pro3-1.py
+++ pro3-1.py
@@ -4,7 +4,7 @@ import os
 ## def clearConsole 
 
 def clear_console():
-   os.system('cls')
+   os.system('clear')
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
